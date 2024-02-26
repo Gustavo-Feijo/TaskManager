aaceeda341a638854f3d393747e2330262586781
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2024-02-26 15:08:28 -0300

    NavBar with 4 links to different pages, yet to add hover animations

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import style from "./NavBar.module.css";
+function NavBar() {
+  const buttons = ["Home", "Projects", "New Project", "About"];
+  return (
+    <nav className="w-full flex items-center justify-around h-20 bg-black border-[color:var(--border-color)] border-b-2 px-48">
+      <div className="flex w-auto">
+        {buttons.map((buttonText, index) => (
+          <Link
+            key={index}
+            href={`/${buttonText.replace(/\s/g, "").toLowerCase()}`}
+            className="min-w-40 flex items-center justify-center border-[color:var(--border-color)] border-2 h-12 mx-5"
+          >
+            {buttonText}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
