674c527f7da05fe13e61b57c9c1bb478b0643733
Gustavo-Feijo <gustavofeijo.dev@gmail.com>
2024-02-26 15:24:27 -0300

    Improve create statements, add start date and deadlines

--- app/database/createstatements.sql
+++ app/database/createstatements.sql
@@ -1,8 +1,15 @@
 create table tb_project(
     project_id int auto_increment primary key,
     project_name varchar(50) not null unique,
-    project_description tinytext,
-    project_status enum("Completed", "On Hold", "In Progress", "Todo") DEFAULT "TODO"
+    project_description tinytext not null,
+    project_status enum("Completed", "On Hold", "In Progress", "Todo") DEFAULT "TODO",
+    project_start_date date,
+    project_deadline date
+);
+
+create table tb_user(
+    user_id int auto_increment primary key,
+    user_name varchar(50) not null
 );
 
 create table tb_task(
@@ -13,11 +20,8 @@ create table tb_task(
     task_user int,
     task_status enum("Completed", "On Hold", "In Progress", "Todo") DEFAULT "Todo",
     task_urgency enum("Critical", "Urgent", "Alert", "Normal", "Low") DEFAULT "Normal",
+    task_start_date date,
+    task_deadline date,
     foreign key(project_id) references tb_project(project_id),
     foreign key(task_user) references tb_user(user_id)
-);
-
-create table tb_user(
-    user_id int auto_increment primary key,
-    user_name varchar(50) not null
 );
